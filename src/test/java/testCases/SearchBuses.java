4b88cd93b406e23fe49b0bf97bb8bc0a1469a896
P-R-I-16 <100308683+P-R-I-16@users.noreply.github.com>
2022-05-25 15:13:53 -0700

    Update SearchBuses.java

--- src/test/java/testCases/SearchBuses.java
+++ src/test/java/testCases/SearchBuses.java
@@ -30,21 +30,6 @@ clickElementByXpath("//*[@id=\"SW\"]/div[1]/div[2]/div[2]/section/form/div[2]/bu
 clickElementByXpath("//li[@data-cy='menu_Buses']/a");
 Thread.sleep(1500);
 clickElementByXpath("//*[@id=\"SW\"]/div[1]/div[2]/div/div/nav/ul/li[6]/a/span[2]");
-/*
-clickElementByXpath("//input[@type='text']");
-Thread.sleep(3000);
-clickElementByXpath("//*[@id='fromCity']/html/body/div/div/div[2]/div/div/div[2]/div[1]/div[1]/label/input");
-enterTextByXpath("//input[@aria-controls='react-autowhatever-1']", "Delhi");
-Thread.sleep(3000);
-clickElementByXpath("(//div[@class='calc60'])[1]");
-clickElementByXpath("//*[@id='toCity']/div/div[2]/div/div/div[2]/div[1]/div[2]/label");
-enterTextByXpath("//input[@aria-controls=\"react-autowhatever-1\"]", "Kanpur, Uttar Pradesh");
-Thread.sleep(3000);
-clickElementByXpath("(//li[@aria-selected='false'])[1]/div/img");
-clickElementByXpath("//*[@id='travelDate']/html/body/div/div/div[2]/div/div/div[2]/div[1]/div[3]/label/input");
-clickElementByXpath("//p[text()='9']");
-*/
-
 clickElementByXpath("//*[@id=\"search_button\"]");
 Thread.sleep(3000);
 
